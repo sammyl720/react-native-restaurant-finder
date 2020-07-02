11025191f0382aae492444d6b2621c27a2dbeb31
sam leider <sam.leider@hotmail.com>
2020-07-02 00:24:08 -0400

    added geolocation

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -4,6 +4,8 @@ import yelp from '../api/yelp'
 export default () => {
   const [results, setResults] = useState([])
   const [errorMessage, setErrorMessage] = useState('')
+  const [coords, setCoords] = useState({ latitude: 0, longitude: 0 })
+
   const searchApi = async (searchTerm) => {
     console.log('hi there')
     try {
@@ -11,7 +13,8 @@ export default () => {
         params: {
           limit: 50,
           term: searchTerm,
-          location: 'crown heights'
+          latitude: coords.latitude,
+          longitude: coords.longitude
         }
       })
       setResults(response.data.businesses)
@@ -27,6 +30,19 @@ export default () => {
   // searchApi('Pizza')
 
   useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const {latitude, longitude } = position.coords
+      setCoords({
+        latitude,
+        longitude
+      })
+    }, (err) => {
+      setErrorMessage(err.message)
+    }, {
+      enableHighAcurracy: true,
+      timeout: 20000,
+      maximumAge: 60 * 1000
+    })
     searchApi('Pasta')
   }, [])
   return [searchApi, results, errorMessage]
